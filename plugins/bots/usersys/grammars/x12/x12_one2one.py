2853834f02cf5136380ed5febb5bbeb5e32a813a
Djura <djura.smits@eonics.nl>
2020-01-20 14:50:37 +0100

    I think I fixed parsing input for my custom plugin Translation isn't correct yet, that will be the next step

---
+++ plugins/bots/usersys/grammars/x12/x12_one2one.py
@@ -0,0 +1,8 @@
+from bots.botsconfig import *
+from .envelope import recorddefs, structure, nextmessage
+
+nextmessage = ({'BOTSID': 'ISA'},)
+
+syntax = {
+    'version': '00403',  # only for sending
+}
