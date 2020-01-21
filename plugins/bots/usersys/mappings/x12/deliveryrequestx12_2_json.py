cd703471e9012a08d31c42cce7720a6167e004e6
Djura <djura.smits@eonics.nl>
2020-01-21 11:31:16 +0100

    PLugin finally works!

--- plugins/bots/usersys/mappings/x12/deliveryrequestx12_2_json.py
+++ plugins/bots/usersys/mappings/x12/deliveryrequestx12_2_json.py
@@ -1,6 +1,16 @@
 def main(inn, out):
+    out.put({'BOTSID' 'deliveryRequest'})
+    loop = inn.getloop({'BOTSID': 'ISA'}, {'BOTSID': 'GS'}, {'BOTSID': 'ST'}, {'BOTSID': 'N1'})
+    loop = list(loop)
 
+    print(loop)
 
-    for partyIn in inn.getloop({'BOTSID': 'ISA'}, {'BOTSID': 'ST'}, {'BOTSID': 'N1'}):
-        partyOut = out.putloop({'BOTSID': 'deliveryRequest'}, {'BOTSID': 'partyName'})
-        partyOut.put({'BOTSID': 'party', 'name': partyIn.get({'BOTSID': 'N1', 'N102': None})})
+    for partyIn in loop:
+        print('New loop...')
+        try:
+            partyOut = out.putloop({'BOTSID': 'deliveryRequest'}, {'BOTSID': 'parties'}, {'BOTSID': 'party'})
+            partyOut.put({'BOTSID': 'party', 'name': partyIn.get({'BOTSID': 'N1', 'N102': None})})
+        except Exception as e:
+            print(e)
+
+    out.root.display()
